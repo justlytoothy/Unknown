f4fe09950feb2529286c8b7c1751f958a28f8219
Jake Schilling <jake@schillingcentral.net>
2022-10-23 13:01:57 -0600

    d

--- api.js
+++ api.js
@@ -1,9 +1,21 @@
 import express from 'express'
-
+import conn from './conn.js'
 const router = express.Router()
 
 router.get('/example', (req, res) => {
 	console.log('accessed')
+	conn.connect(function (err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack)
+			return
+		}
+
+		console.log('connected as id ' + connection.threadId)
+	})
+	conn.query('SELECT * FROM tasks', (err, rows, fields) => {
+		console.log(rows, fields)
+	})
+	conn.end()
 	return res.json({ bruh: 'here' })
 })
 
---
+++ conn.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql'
+console.log({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+})
+const conn = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+})
+export default conn
